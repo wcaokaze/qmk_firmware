4e044317f15ef7ba4b275798271b557c104379fc
wcaokaze <wcaokaze@gmail.com>
2020-11-11 02:04:34 +0900

    1st prototype

---
+++ keyboards/ninja60/config.h
@@ -0,0 +1,53 @@
+/* Copyright 2020 wcaokaze
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x217A
+#define PRODUCT_ID      0x6000
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    wcaokaze
+#define PRODUCT         ninja60
+#define DESCRIPTION     'wcaokaze\'s keyboard'
+
+/* key matrix size */
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 7
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+*/
+#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2 }
+#define UNUSED_PINS
+
+#define ENCODERS_PAD_A { E6, B5 }
+#define ENCODERS_PAD_B { B4, B6 }
+
+#define DIODE_DIRECTION COL2ROW
+
+#define SOFT_SERIAL_PIN D3
+#define SPLIT_HAND_PIN D2
---
+++ keyboards/ninja60/keymaps/default/keymap.c
@@ -0,0 +1,127 @@
+/* Copyright 2020 wcaokaze
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+  _DVORAK = 0,
+  _NORMAL_SPC,
+  _NUM,
+  _SYMBOL,
+  _FN
+};
+
+enum encoder_number {
+  _1ST = 0,
+  _2ND,
+};
+
+enum custom_keycodes {
+   S_ARW = SAFE_RANGE,
+   D_ARW
+};
+
+#define TG_SPC TG(_NORMAL_SPC)
+#define SYM_SPC LT(_SYMBOL, KC_SPC)
+#define BS_SFT SFT_T(KC_BSPC)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_DVORAK] = LAYOUT(
+      KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_F3  ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_SLSH, KC_BSLS,
+      KC_TAB , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   , KC_F6  ,     KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_MINS, KC_EQL ,
+      KC_LCTL, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   , KC_KANA,     KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_LBRC, KC_RBRC,
+      KC_LSFT, KC_Z   , KC_Q   , KC_J   , KC_K   , KC_X   ,              KC_B   , KC_M   , KC_W   , KC_V   , KC_SCLN, S_ARW  , TG(_NUM),
+      MO(_FN)       , KC_LALT    , KC_SPC   , SYM_SPC     , KC_ESC ,     BS_SFT , KC_ENT , KC_ENT     , KC_RGUI
+  ),
+
+  [_NORMAL_SPC] = LAYOUT(
+      _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______, _______, _______,
+      _______       , _______    , _______  , KC_SPC      , _______,     _______, _______, _______    , _______
+  ),
+
+  [_NUM] = LAYOUT(
+      _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,     _______, KC_P7  , KC_P8  , KC_P9  , _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,     _______, KC_P4  , KC_P5  , KC_P6  , _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,              KC_P0  , KC_P1  , KC_P2  , KC_P3  , _______, _______, _______,
+      _______       , _______    , _______  , _______     , _______,     _______, _______, _______    , _______
+  ),
+
+  [_SYMBOL] = LAYOUT(
+      _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
+      _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, _______,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_QUES, _______,
+      _______, _______, _______, _______, _______, _______,              _______, _______, _______, _______, _______, D_ARW  , _______,
+      _______       , _______    , _______  , _______     , _______,     _______, _______, _______    , _______
+  ),
+
+  [_FN] = LAYOUT(
+      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , _______,     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
+      _______, _______, _______, _______, _______, _______, _______,     _______, _______, KC_PSCR, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,              _______, KC_HOME, KC_END , _______, _______, D_ARW, _______,
+      _______       , _______    , _______  , _______     , TG_SPC ,     KC_DEL , _______, _______    , _______
+  )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+   switch (keycode) {
+      case S_ARW:
+         if (record->event.pressed) {
+            SEND_STRING("->");
+         }
+         return true;
+      case D_ARW:
+         if (record->event.pressed) {
+            SEND_STRING("=>");
+         }
+         return true;
+      default:
+         return true;
+   }
+}
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+  switch (index) {
+    case _1ST:
+      if (clockwise) {
+        tap_code(KC_F2);
+      } else {
+        tap_code(KC_F4);
+      }
+      break;
+    case _2ND:
+      if (clockwise) {
+        tap_code(KC_F5);
+      } else {
+        tap_code(KC_F7);
+      }
+      break;
+  }
+}
+
+bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+   switch (keycode) {
+      case SYM_SPC:
+         return true;
+      default:
+         return false;
+   }
+}
+
