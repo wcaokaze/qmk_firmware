e1a549331137792f02a47fd359fd147c342d6ec5
wcaokaze <wcaokaze@gmail.com>
2020-11-13 03:06:38 +0900

    added Fn key on the right side

--- keyboards/ninja60/keymaps/default/keymap.c
+++ keyboards/ninja60/keymaps/default/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   , KC_LSFT,     KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_MINS, KC_EQL ,
       KC_LCTL, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   , KC_KANA,     KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_LBRC, KC_RBRC,
       KC_LSFT, KC_Z   , KC_Q   , KC_J   , KC_K   , KC_X   ,              KC_B   , KC_M   , KC_W   , KC_V   , KC_SCLN, S_ARW  , TG(_NUM),
-      MO(_FN)      , KC_LALT   , KC_SPC     , SYM_SPC     , KC_ESC ,     BS_SFT , KC_ENT     , KC_ENT      , KC_RGUI
+      MO(_FN)      , KC_LALT   , KC_SPC     , SYM_SPC     , KC_ESC ,     BS_SFT , KC_ENT     , MO(_FN)     , KC_RGUI
   ),
 
   [_NORMAL_SPC] = LAYOUT(
