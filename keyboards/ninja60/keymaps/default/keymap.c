1360cde2e9c3ae6cdf31f1cb00645e4da9589774
wcaokaze <wcaokaze@gmail.com>
2021-02-08 16:36:58 +0900

    swapped ESC and backspace

--- keyboards/ninja60/keymaps/default/keymap.c
+++ keyboards/ninja60/keymaps/default/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB , KC_QUOT, KC_COMM, KC_DOT , KC_P   , KC_Y   , KC_LSFT,     KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_EQL , XXXXXXX,
       KC_LCTL, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   , KC_KANA,     KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_LBRC, XXXXXXX,
       KC_LSFT, KC_Z   , KC_Q   , KC_J   , KC_K   , KC_X   ,              KC_B   , KC_M   , KC_W   , KC_V   , KC_SCLN, KC_MINS, TG(_NUM),
-      MO(_FN)      , KC_LALT   , UNDS_SPC   , SYM_SPC     , ESC_SFT,     KC_BSPC, KC_ENT     , MO(_FN)     , KC_RGUI
+      MO(_FN)      , KC_LALT   , UNDS_SPC   , SYM_SPC     , KC_BSPC,     ESC_SFT, KC_ENT     , MO(_FN)     , KC_RGUI
   ),
 
   [_NORMAL_SPC] = LAYOUT(
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, _______, _______, _______, _______, _______,     _______, _______, KC_PSCR, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,              _______, KC_HOME, KC_END , _______, _______, D_ARW, _______,
-      _______      , _______   , _______    , _______     , TG_SPC ,     _______, _______    , _______     , _______
+      _______      , _______   , _______    , KC_SPC      , TG_SPC ,     _______, _______    , _______     , _______
   )
 };
 
